6a5ec8440c9bfbcbd06c15b3c6d422f21ff0e231
Andrew Grogan-Kaylor <agrogan@umich.edu>
2022-12-28 11:00:00 -0500

    sorted bibliography

--- updates.R
+++ updates.R
@@ -1,12 +1,22 @@
+# sort bibliography
+
+library(RefManageR)
+
+mybib <- ReadBib("multilevel-thinking.bib")
+
+mybib <- sort(mybib, sorting = "nyt") # sort by name - year - title
+
+WriteBib(mybib, file = "multilevel-thinking.bib")
+
 # use pandoc to conver to PDF
 
-getwd()
-
-library(rmarkdown)
-
-list.files (path = "./docs")
-
-
-pandoc_convert(input = "/Users/agrogan/Desktop/GitHub/multilevel-thinking/docs/Multilevel-Thinking.docx",
-               # to = "pdf",
-               output = "/Users/agrogan/Desktop/GitHub/multilevel-thinking/docs/Multilevel-Thinking.pdf")
+# getwd()
+# 
+# library(rmarkdown)
+# 
+# list.files (path = "./docs")
+# 
+# 
+# pandoc_convert(input = "/Users/agrogan/Desktop/GitHub/multilevel-thinking/docs/Multilevel-Thinking.docx",
+#                # to = "pdf",
+#                output = "/Users/agrogan/Desktop/GitHub/multilevel-thinking/docs/Multilevel-Thinking.pdf")
